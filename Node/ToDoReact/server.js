67bb0f2ec5f466ef66f58a9860a8f89252d0d63d
lucas oliveira <lucashbo321@gmail.com>
2025-01-24 21:39:44 -0300

    Removendo node_modules

---
+++ Node/ToDoReact/server.js
@@ -0,0 +1,65 @@
+
+import dotenv from 'dotenv';
+dotenv.config({path: "C:\Users\lucas\Desktop\cursos\javaScript\API\Node\ToDoReact\infodb.env"});
+import express from 'express';
+import cors from 'cors';
+import mysql2 from 'mysql2';
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+
+const db = mysql2.createConnection({
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST
+})
+
+db.connect((err) =>{
+    if(err){
+        console.log('erro de conexão do bd!!')
+    }else{
+        console.log('banco de dados conectado!')
+    }
+})
+
+app.get('/', (req, res)=>{
+    res.send('valid, json')
+})
+
+app.post('/Login', (req, res)=>{
+    try {
+        const {user, pass} = req.body;
+        if(!user || !pass){
+            req.status(400).json({"erro": "usuário e senha requeridas"})
+        }
+
+        const sql = "SELECT * FROM logins WHERE username = ? and password = ?"
+
+        db.query(sql, [user, pass], (err, results) =>{
+            if (err) {
+               console.error('Erro ao executar a query:', err);
+               return res.status(500).json({ error: 'Erro no servidor' });
+            }
+          
+              if (results.length > 0) {
+                res.status(200).json({ message: 'Usuário encontrado', user: results[0] });
+            } else {
+               res.status(404).json({ error: 'Usuário ou senha incorretos' });
+            }
+        })
+    //consultar no banco de dados 
+    } catch (error) {
+       cconsole.error('Erro interno:', error);
+       res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+    })
+
+app.listen(port, ()=>{
+    console.log(`Server on In port ${port}`);
+})
---
+++ Node/example/api.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import mysql2 from 'mysql2'
+const app = express();
+const port = 5000;
+app.use(express.json())
+
+const db = mysql2.createConnection({
+    user: 'u224972953_root',
+    database: 'u224972953_teste_php',
+    password: 'Skoi7617@',
+    host: '62.72.62.1'
+});
+
+db.connect((err)=>{
+    if(err){
+        console.error('erro ao conectar ao bd: ', err)
+    }else{
+        console.log('Banco de dados conectado com sucesso!!, só alegria')
+    }
+})
+
+app.get('/',(req, res) => {
+    res.send('Olá, mundo!')
+})
+
+app.post('/post', (req, res)=>{
+    const {valor1, valor2, valor3, valor4, valor5} = req.body;
+    try{
+        console.log(valor1, valor2, valor3, valor4)
+        res.status(200).send({valores: "recebidos"})
+    }catch{
+        console.error('erro ao exibir dados recebidos')
+        res.status(500).send({erro: 'erro de consulta'})
+    }finally{
+        console.log('fim da execução')
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`servidor ta online na porta ${port}`)
+})
