dab757f4823a96f190f27120884b8fda87dcbe8f
lucas oliveira <lucashbo321@gmail.com>
2025-01-26 12:51:20 -0300

    melhoria nas requisiçoes existentes e adição do Delete

--- Node/ToDoReact/server.js
+++ Node/ToDoReact/server.js
@@ -17,31 +17,43 @@ const db = mysql2.createConnection({
     host: process.env.DB_HOST
 })
 
-db.connect((err) =>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         console.log('erro de conexão do bd!!')
-    }else{
+    } else {
         console.log('banco de dados conectado!')
     }
 })
 
-app.get('/', (req, res)=>{
-    res.send('valid, json')
+
+app.get('/', (req, res) => {
+    try {
+        const sql = "SELECT * FROM task"
+        db.query(sql, (err, results) => {
+            if (err) {
+                res.status(500).json({ erro: "erro de banco de dados" })
+            }
+            res.status(200).send(results);
+        })
+    } catch (error) {
+        console.log("erro de servidor", error)
+        res.status(500).send({ erro: "erro de servidor" })
+    }
 })
 
-app.put('/Login',(req,res)=>{
+app.post('/Login', (req, res) => {
     try {
-        const {usuario, pass} = req.body;
-        if(!usuario || !pass){
-            res.status(400).json({erro: "usuario ou senha inválidos"})
+        const { usuario, pass } = req.body;
+        if (!usuario || !pass) {
+            res.status(400).json({ erro: "usuario ou senha inválidos" })
         }
-        
+
         const sql = "INSERT INTO logins value(?, ?, ?)"
         //const id = Math.random()
         db.query(sql, ['id', pass, usuario], (err, results) => {
-            if(err){
+            if (err) {
                 console.log("erro ao executar query:", err);
-                return res.status(500).json({erro: "Erro de Servidor"});
+                return res.status(500).json({ erro: "Erro de Servidor" });
             }
             console.log(results)
             return res.sendStatus(200)
@@ -49,36 +61,75 @@ app.put('/Login',(req,res)=>{
 
     } catch (error) {
         console.log("erro interno", error.message)
-        res.status(500).json({erro: "erro interno do servidor"});
+        res.status(500).json({ erro: "erro interno do servidor" });
     }
 })
 
-app.post('/Login', (req, res)=>{
+app.post('/addTask', (req, res) => {
     try {
-        const {usuario, pass} = req.body;
-        if(!usuario || !pass){
-            res.status(400).json({"erro": "usuário e senha requeridas"})
+        const { novaTarefa, deadline } = req.body;
+        if (!novaTarefa) {
+            res.status(400)
+        }
+
+        const sql = "INSERT INTO task(task, deadline) VALUES (?, ?)"
+
+        db.query(sql, [novaTarefa, deadline], (err, results) => {
+            if (err) {
+                console.log("erro de query nas tasks:", err);
+                return res.status(500).json({ erro: err })
+            }
+            return res.status(200).json({ sucess: "Task Adicionada" })
+        })
+    } catch (error) {
+        console.log("erro no servidor")
+        res.status(500).json({ erro: "erro de servidor" })
+    }
+})
+
+app.delete('/Delete', (req, res) => {
+    try {
+        const { tarefaParaRemover } = req.body;
+        const sql = "DELETE FROM task WHERE id = ?";
+        db.query(sql, [tarefaParaRemover], (err, results) => {
+            if (err) {
+                console.log(err)
+                return res.status(400).json({ erro: err })
+            }
+
+            res.status(200).json({ sucess: "DELETADO COM SUCESSO" });
+        })
+    }catch(err){
+        res.status(500).json({erro: "Erro de servidor"})
+    }
+})
+
+app.post('/Login', (req, res) => {
+    try {
+        const { usuario, pass } = req.body;
+        if (!usuario || !pass) {
+            res.status(400).json({ "erro": "usuário e senha requeridas" })
         }
         const sql = "SELECT * FROM logins WHERE user = ? AND pass = ?"
 
-        db.query(sql, [usuario, pass], (err, results) =>{
+        db.query(sql, [usuario, pass], (err, results) => {
             if (err) {
-               console.error('Erro ao executar a query:', err);
-               return res.status(500).json({ error: 'Erro no servidor' });
+                console.error('Erro ao executar a query:', err);
+                return res.status(500).json({ error: 'Erro no servidor' });
             }
-          
+
             if (results.length > 0) {
-                res.status(200).json({ message: 'Success'});
+                res.status(200).json({ message: 'Success' });
             } else {
-               res.status(404).json({ error: 'Usuário ou senha incorretos' });
+                res.status(404).json({ error: 'Usuário ou senha incorretos' });
             }
         })
     } catch (error) {
-       console.error('Erro interno:', error);
-       res.status(500).json({ error: 'Erro interno do servidor' });
+        console.error('Erro interno:', error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
     }
-    })
+})
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server on In port ${port}`);
 })
