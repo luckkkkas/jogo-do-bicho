27a17860f836aa9786a0434b9e33fa0607b4b3a5
lucas oliveira <lucashbo321@gmail.com>
2025-01-27 20:21:37 -0300

    correção da conexão ficar encerrando

--- Node/ToDoReact/server.js
+++ Node/ToDoReact/server.js
@@ -10,26 +10,20 @@ dotenv.config();
 app.use(express.json());
 const port = process.env.PORT;
 
-const db = mysql2.createConnection({
+const pool = mysql2.createPool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     user: process.env.DB_USER,
-    host: process.env.DB_HOST
-})
-
-db.connect((err) => {
-    if (err) {
-        console.log('erro de conexão do bd!!')
-    } else {
-        console.log('banco de dados conectado!')
-    }
-})
-
+    host: process.env.DB_HOST,
+    waitForConnections: true,
+    connectionLimit: 10, // Número máximo de conexões no pool
+    queueLimit: 0        // Sem limite de requisições na fila
+});
 
 app.get('/', (req, res) => {
     try {
         const sql = "SELECT * FROM task"
-        db.query(sql, (err, results) => {
+        pool.query(sql, (err, results) => {
             if (err) {
                 res.status(500).json({ erro: "erro de banco de dados" })
             }
@@ -50,7 +44,7 @@ app.post('/CreateUser', (req, res) => {
 
         const sql = "INSERT INTO logins value(?, ?, ?)"
         //const id = Math.random()
-        db.query(sql, ['id', usuario, pass], (err, results) => {
+        pool.query(sql, ['id', usuario, pass], (err, results) => {
             if (err) {
                 console.log("erro ao executar query:", err);
                 return res.status(500).json({ erro: "Erro de Servidor" });
@@ -74,7 +68,7 @@ app.post('/addTask', (req, res) => {
 
         const sql = "INSERT INTO task(task, deadline) VALUES (?, ?)"
 
-        db.query(sql, [novaTarefa, deadline], (err, results) => {
+        pool.query(sql, [novaTarefa, deadline], (err, results) => {
             if (err) {
                 console.log("erro de query nas tasks:", err);
                 return res.status(500).json({ erro: err })
@@ -91,7 +85,7 @@ app.delete('/Delete', (req, res) => {
     try {
         const { tarefaParaRemover } = req.body;
         const sql = "DELETE FROM task WHERE id = ?";
-        db.query(sql, [tarefaParaRemover], (err, results) => {
+        pool.query(sql, [tarefaParaRemover], (err, results) => {
             if (err) {
                 console.log(err)
                 return res.status(400).json({ erro: err })
@@ -112,7 +106,7 @@ app.post('/Login', (req, res) => {
         }
         const sql = "SELECT * FROM logins WHERE user = ? AND pass = ?"
 
-        db.query(sql, [usuario, pass], (err, results) => {
+        pool.query(sql, [usuario, pass], (err, results) => {
             if (err) {
                 console.error('Erro ao executar a query:', err);
                 return res.status(500).json({ error: 'Erro no servidor' });
